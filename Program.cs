f2f45279855556d35a919654c26e6601bd427979
T-SaiKumar <saikumar.thathineni@sailssoftware.com>
2021-11-19 12:17:14 +0530

    TSk

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SingleTonPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SingleObject first = SingleObject.GetInstance();
+            SingleObject second = SingleObject.GetInstance();
+
+            if (first == second)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+
+
+              first.ShowMessage();
+              second.Message();
+        }
+    }
+}
---
+++ SingleObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTonPattern
+{
+    public class SingleObject
+    {
+        private static readonly SingleObject instance = new();
+
+        private SingleObject() { }
+
+        public static SingleObject GetInstance()
+        {
+            return instance;
+        }
+
+        public  void ShowMessage()
+        {
+            Console.WriteLine ("This is singleton design pattern");
+        }
+
+        public void Message()
+        {
+            Console.WriteLine("This is also a singleton design pattern");
+        }
+
+    }
+}
